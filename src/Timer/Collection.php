38cb732e9fe3a5cce456175b06662702addf8dc9
Wallace <wallacemaxters@gmail.com>
2015-02-24 14:24:42 -0300

    ...

---
+++ src/Timer/Collection.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace WallaceMaxters\Timer;
+
+use ArrayAcess
+use IteratorAggregate;
+
+class Collection implements ArrayAccess, IteratorAggregate
+{
+
+	protected $items = [];
+
+	protected $format = null;
+    
+    /**
+    * @param array $times
+    * @param string $format = Default format for all items of collection
+    */
+    public function __construct(array $times = [], $format = null)
+    {
+    	$this->format = $format;
+
+        foreach($times as $key => $time) {
+            
+            $this[$key] = new Time(0, 0, $time);
+        }
+    }
+
+	public function setTime($key, Time $time)
+	{
+		$time->setFormat($this->format);
+
+		if ($key === null) {
+
+			$this->items[] = $time;
+
+		} else {
+
+			$this->items[$key] = $time;
+
+		}
+
+		return $this;
+	}
+
+	public function offsetSet($key, $value)
+	{
+		return $this->setTime($key, $value);
+	}
+
+	public function offsetUnset($key)
+	{
+		unset($this->items[$key]);
+	}
+
+	public function offsetExists($key)
+	{
+		return array_key_exists($key, $this->items);
+	}
+
+	public function offsetGet($key)
+	{
+		if ($this->offsetExists($key)) {
+
+			return $this->items[$key];
+		} 
+
+		return null;
+	}
+
+	public function getIterator()
+	{
+		return new ArrayIterator($this->items);
+	}
+
+	public function sortAsc()
+	{	
+
+		asort($this->items);
+
+		return $this;
+	}
+
+	public function sortDesc()
+	{
+		arsort($this->items);
+
+		return $this;
+	}
+
+}
---
+++ src/Timer/Time.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace WallaceMaxters\Timer;
+
+class Time
+{
+	protected $aliases = [
+		'%h', '%i', '%s'
+	];
+
+	protected $seconds = 0;
+
+	protected $format = '%h:%i:%s';
+
+	
+	public function __construct($hours = 0, $minutes = 0, $seconds = 0)
+	{
+		$this->setTime($hours, $minutes, $seconds);
+	}
+
+	public function setTime($hours, $minutes, $seconds)
+	{
+
+		$this->seconds = intval($hours) * 3600;
+		$this->seconds += intval($minutes) * 60;
+		$this->seconds += intval($seconds);
+
+		return $this;
+	}
+
+	public function setSeconds($seconds)
+	{
+		return $this->setTime(0, 0, $seconds);
+	}
+
+	public function setMinutes($minutes)
+	{
+		return $this->setTime(0, $minutes, 0);
+	}
+
+	public function setHours($hours)
+	{
+		return $this->setTime($hours, 0, 0);
+	}
+
+	public function addSeconds($seconds)
+	{
+		return $this->setTime(0, 0, $this->seconds + intval($seconds));
+	}
+
+	public function addMinutes($minutes)
+	{
+		return $this->setTime(0, intval($minutes), $this->seconds);
+	}
+
+	public function addHours($hours)
+	{
+		return $this->setTime(intval($hours), 0, $this->seconds);
+	}
+
+
+	public function format($format)
+	{
+		$hours = floor($this->seconds / 3600);
+
+		$minutes = floor(($this->seconds - ($hours * 3600)) / 60);
+
+		$seconds = floor($this->seconds % 60);
+
+		$elements = array_map([$this, 'zeroPadding'], compact('hours', 'minutes', 'seconds'));
+
+		return str_replace($this->aliases, $elements, $format);
+	}
+
+
+	public function setFormat($format)
+	{
+		$this->format = (string) $format;
+
+		return $this;
+	}
+
+
+	public function __toString()
+	{
+		return $this->format($this->format);
+	}
+
+
+	protected function zeroPadding($value)
+	{
+		return sprintf('%02s', $value);
+	}
+}
