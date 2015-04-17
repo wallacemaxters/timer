0944e77ecd072cb82f2799860390c889faf4adf2
= <wallacemaxters@gmail.com>
2015-04-17 13:13:45 -0300

    adicionado o arquivo Parser.php

---
+++ src/WallaceMaxters/Timer/Parser.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace WallaceMaxters\Timer;
+
+use InvalidArgumentException;
+
+class Parser
+{
+    protected $time;
+
+    protected $formatReplaces = [
+        Time::HOUR_FORMAT   => '\d{1,2}',
+        Time::MINUTE_FORMAT => '\d{1,2}',
+        Time::SECOND_FORMAT => '\d{1,2}',
+    ];
+
+    public function __construct()
+    {
+        $this->time = new Time;
+    }
+
+    /**
+    * @return \WallaceMaxters\Timer\Time
+    */
+    public function getTime()
+    {
+        return $this->time;
+    }
+
+    /**
+    * @access public
+    * @param string $format
+    * @param string $value
+    * @param string $separator = Format of separator
+    * @return \WallaceMaxters\Timer\Time
+    * @throws \InvalidArgumentException
+    */
+
+    public function parseFormat($format, $value, $separator = ':')
+    {
+        $regexPart = strtr(preg_quote($format), $this->formatReplaces);
+
+        if (! preg_match("/^{$regexPart}$/", $value)) {
+
+            throw new InvalidArgumentException('Non-compatible format in comparsion with value');
+        }
+
+        $values = array_map('intval', explode($separator, $value));
+
+        $formats = array_map('trim', explode($separator, $format));
+
+        $combined = array_combine($formats, $values);
+
+        if (isset($combined[Time::HOUR_FORMAT])) {
+
+            $this->time->addHours($combined[Time::HOUR_FORMAT]);
+        }
+
+        if (isset($combined[Time::MINUTE_FORMAT])) {
+
+            $this->time->addMinutes($combined[Time::MINUTE_FORMAT]);
+        }
+
+
+        if (isset($combined[Time::SECOND_FORMAT])) {
+
+            $this->time->addSeconds($combined[Time::SECOND_FORMAT]);  
+        }
+
+        return $this;
+    }
+
+}
