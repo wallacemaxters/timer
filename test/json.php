a62ffb4546b45fb43457e8740c88e3e0bff5acd2
= <wallacemaxters@gmail.com>
2015-12-04 12:48:23 -0200

    JsonSerialize implementation

---
+++ test/json.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once __DIR__ . '/../vendor/autoload.php';
+
+use WallaceMaxters\Timer\Collection;
+
+
+$collection = Collection::create()
+						->push(50)
+						->push(10)
+						->mergeArray([10, 50, 700])
+						->push(100);
+
+
+
+var_dump(json_encode($collection, JSON_PRETTY_PRINT));
+
+var_dump(json_encode($collection->sum(), JSON_PRETTY_PRINT));
+
